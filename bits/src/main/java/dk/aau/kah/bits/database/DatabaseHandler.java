9e4aa409fc8000f28c7c9b25f00ebe80291ec479
Kim A. Jakobsen <kimajakobsen@gmail.com>
2015-08-28 10:26:04 +0200

    Adapt to the new way to get filenames in the databaseConfig file.

--- bits/src/main/java/dk/aau/kah/bits/database/DatabaseHandler.java
+++ bits/src/main/java/dk/aau/kah/bits/database/DatabaseHandler.java
@@ -1,9 +1,19 @@
 package dk.aau.kah.bits.database;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.jena.propertytable.PropertyTable;
 import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFDataMgr;
@@ -22,12 +32,12 @@ public class DatabaseHandler {
 		databaseConfig.validate();
 		this.databaseConfig = databaseConfig;
 		
-		if (this.databaseConfig.doFreshLoad()) {
+		if (this.databaseConfig.isFreshLoad()) {
 			clearTDBDatabase();
 		}
 		
 		if (this.databaseConfig.getExperimentDataset().equals("TPC-H")) {
-			loadTPCHExperimentalDataset();
+			loadTPCHDataset();
 		}
 		else if (this.databaseConfig.getExperimentDataset().equals("TPC-DS")) {
 			//TODO
@@ -37,8 +47,8 @@ public class DatabaseHandler {
 		}
 	}
 	
-	private void loadTPCHExperimentalDataset() {
-		String directory = databaseConfig.getTDBPath() ;
+	private void loadTPCHDataset() {
+		String directory = databaseConfig.getTDBFilePath() ;
 		this.dataset = TDBFactory.createDataset(directory);
 		
 		this.dataset.begin(ReadWrite.WRITE) ;
@@ -48,30 +58,56 @@ public class DatabaseHandler {
 			Model dimensions = ModelFactory.createDefaultModel();
 			Model facts = ModelFactory.createDefaultModel();
 			
+			
 			ontology.add(RDFDataMgr.loadModel("src/main/resources/"+databaseConfig.getExperimentDataset()+"/onto/tpc-h-qb4o-delivered-version.ttl"));
-			dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/customer.ttl"));
-			dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/nation.ttl"));
-			dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/orders.ttl"));
-			dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/part.ttl"));
-			dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/partsupp.ttl"));
-			dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/region.ttl"));
-			dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/supplier.ttl"));
-			facts.add(RDFDataMgr.loadModel(getTPCHPath()+"/lineitem.ttl"));
+	
+			if (databaseConfig.getDimensionModelName().equals("st")) {
+				dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/customer.ttl"));
+				dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/nation.ttl"));
+				dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/orders.ttl"));
+				dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/part.ttl"));
+				dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/partsupp.ttl"));
+				dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/region.ttl"));
+				dimensions.add(RDFDataMgr.loadModel(getTPCHPath()+"/supplier.ttl"));
+			} else if (databaseConfig.getDimensionModelName().equals("pt")) {
+				//PropertyTable customerPropertyTable = initilizePropertyTable(ontology,getTPCHPath()+"/customer.ttl");
+				//TODO
+			}
+
+			if (databaseConfig.getFactModelName().equals("st")) {
+				facts.add(RDFDataMgr.loadModel(getTPCHPath()+"/lineitem.ttl"));
+			} else if (databaseConfig.getFactModelName().equals("pt")) {
+				PropertyTable factPropertyTable = initilizePropertyTable(ontology,getTPCHPath()+"/lineitem.ttl");
+			}
+			
 			
 			//Concatenate the model with the existing model and add it to the named graph. 
 			dataset.addNamedModel(databaseConfig.getOntologyModelURL(), ontology.add(dataset.getNamedModel(databaseConfig.getOntologyModelURL())));
 			dataset.addNamedModel(databaseConfig.getDimensionModelURL(), dimensions.add(dataset.getNamedModel(databaseConfig.getOntologyModelURL())));
 			dataset.addNamedModel(databaseConfig.getFactModelURL(), facts.add(dataset.getNamedModel(databaseConfig.getOntologyModelURL())));
 
-
-		
-		dataset.commit();
-		
+			dataset.commit();
 		} finally {
 			dataset.end();
 		}
 	}
 	
+
+	
+	private PropertyTable initilizePropertyTable(Model ontology, String path) {
+		List<String> destinctProperties;
+		String sparql = "select distinct ?b where {?a ?b ?c}";
+
+		Query qry = QueryFactory.create(sparql);
+		QueryExecution qe = QueryExecutionFactory.create(qry, ontology);
+		ResultSet rs = qe.execSelect();
+		
+		ResultSetFormatter.out(System.out, rs, qry) ;
+		
+		
+		return null;
+	}
+
 	private String getTPCHPath(){
 		return "src/main/resources/"+databaseConfig.getExperimentDataset()+"/"+databaseConfig.getScaleFactorString();
 	}
@@ -106,7 +142,7 @@ public class DatabaseHandler {
 		
 	public void clearTDBDatabase() throws IOException {
 
-			FileUtils.cleanDirectory(databaseConfig.getTDBPathFile());
+			FileUtils.cleanDirectory(new File(databaseConfig.getTDBFilePath()));
 
 	}
 }
