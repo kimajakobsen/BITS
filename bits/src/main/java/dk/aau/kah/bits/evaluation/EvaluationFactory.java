97067f2ab76e397789c0a73ebdbe19923e715b67
Kim A. Jakobsen <kimajakobsen@gmail.com>
2015-09-07 18:05:50 +0200

    add file

---
+++ bits/src/main/java/dk/aau/kah/bits/evaluation/EvaluationFactory.java
@@ -0,0 +1,27 @@
+package dk.aau.kah.bits.evaluation;
+
+import da.aau.kah.bits.exceptions.InvalidDatabaseConfig;
+import dk.aau.kah.bits.database.DatabaseConfig;
+import dk.aau.kah.bits.database.DatabaseHandler;
+
+public class EvaluationFactory {
+
+	public EvaluationFactory() {
+		
+	}
+
+	public AbstractEvaluationHandler makeEvaluation(DatabaseHandler databaseHandle, DatabaseConfig databaseConfig) throws InvalidDatabaseConfig {
+		String datasetType = databaseConfig.getDatasetType();
+		AbstractEvaluationHandler evaluationhandler;
+		
+		if (datasetType.equals("TPC-H")) {
+			evaluationhandler = new EvaluationHandlerTPCH(databaseHandle);
+		} else {
+			throw new InvalidDatabaseConfig("The Dataset Type "+databaseConfig.getDatasetType()+" is not known, implementation is missing.");
+		}
+		
+		
+		return evaluationhandler;
+	}
+	 
+}
