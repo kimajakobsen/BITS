5d60bcb5c57e13d76d79d6566308977c01ea41d5
Kim A. Jakobsen <kimajakobsen@gmail.com>
2015-09-09 16:55:28 +0200

    added getNamedGraphs method and renamed the class

--- bits/src/main/java/dk/aau/kah/bits/evaluation/AbstractExperimentHandler.java
+++ bits/src/main/java/dk/aau/kah/bits/evaluation/AbstractExperimentHandler.java
@@ -2,13 +2,19 @@ package dk.aau.kah.bits.evaluation;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.rdf.model.Resource;
 
 import dk.aau.kah.bits.database.DatabaseHandler;
 
-public abstract class AbstractEvaluationHandler {
+public abstract class AbstractExperimentHandler {
 	protected DatabaseHandler databaseHandler;
 	
-	AbstractEvaluationHandler(DatabaseHandler databaseHandler){
+	AbstractExperimentHandler(DatabaseHandler databaseHandler){
 		this.databaseHandler = databaseHandler;
 	}
 
@@ -17,4 +23,25 @@ public abstract class AbstractEvaluationHandler {
 	public abstract HashMap<String, String> getResults();
 	
 	public abstract void getResultTimes();
+	
+	protected HashSet<String> getNamedGraphs(Query query) {
+		HashSet<String> namedGraphs = new HashSet<String>(); 
+		
+		namedGraphs.add(databaseHandler.getOntologyModelURI());
+		namedGraphs.add(databaseHandler.getFactModelURI());
+		
+		if (databaseHandler.getDimensionModelURI() == null) {
+			HashSet<Resource> levelProperties = databaseHandler.getDistinctLevelProperties();
+			for (Resource resource : levelProperties) {
+				Pattern p = Pattern.compile("(.)*"+resource.toString()+"(.)*", Pattern.DOTALL);
+				Matcher m = p.matcher(query.getQueryPattern().toString());
+				if (m.find()) { //if property is in query
+					namedGraphs.add(resource.toString());
+				}
+			}
+		} else {
+			namedGraphs.add(databaseHandler.getDimensionModelURI());
+		}
+		return namedGraphs;
+	}
 }
