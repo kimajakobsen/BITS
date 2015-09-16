821eeb571b2903a2d59d1809518a1af8fb55e10b
Kim A. Jakobsen <kimajakobsen@gmail.com>
2015-09-16 07:52:11 +0200

    Added the first few methods.

--- bits/src/main/java/da/aau/kah/bits/config/GeneralConfig.java
+++ bits/src/main/java/da/aau/kah/bits/config/GeneralConfig.java
@@ -1,5 +1,37 @@
 package da.aau.kah.bits.config;
 
 public class GeneralConfig {
+	
+	private static GeneralConfig instance = null;
+	protected GeneralConfig() {}
+	
+	private String tdbLoaderPath;
+	private Boolean Verbose = false;
+	
+	public static GeneralConfig getInstance() {
+		if(instance == null) {
+			instance = new GeneralConfig();
+		}
+		return instance;
+	}
 
+
+	public String getTdbLoaderPath() {
+		return instance.tdbLoaderPath;
+	}
+
+	public void setTdbLoaderPath(String tdbLoader) {
+		instance.tdbLoaderPath = tdbLoader;
+	}
+
+	public Boolean isVerbose() {
+		return instance.Verbose;
+	}
+
+	public void setVerbose(Boolean verbose) {
+		instance.Verbose = verbose;
+	}
+
+
+	
 }
