136812f5d08f0452a4f21f7ab2683bf81a83d9b4
Kim A. Jakobsen <kimajakobsen@gmail.com>
2015-09-22 09:10:46 +0200

    Clean up

--- bits/src/main/java/dk/aau/kah/bits/main/Bits.java
+++ bits/src/main/java/dk/aau/kah/bits/main/Bits.java
@@ -2,52 +2,51 @@ package dk.aau.kah.bits.main;
 
 import java.io.IOException;
 
-import org.apache.jena.graph.NodeFactory;
-import org.apache.jena.propertytable.Column;
-import org.apache.jena.propertytable.PropertyTable;
-import org.apache.jena.propertytable.Row;
-import org.apache.jena.propertytable.graph.GraphPropertyTable;
-import org.apache.jena.propertytable.impl.PropertyTableArrayImpl;
-import org.apache.jena.query.Query;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.ResultSet;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
+import da.aau.kah.bits.config.DatabaseConfig;
+import da.aau.kah.bits.config.GeneralConfig;
 import da.aau.kah.bits.exceptions.InvalidDatabaseConfig;
-import dk.aau.kah.bits.database.DatabaseConfig;
 import dk.aau.kah.bits.database.DatabaseHandler;
+import dk.aau.kah.bits.experiment.AbstractExperimentHandler;
+import dk.aau.kah.bits.experiment.ExperimentFactory;
 import dk.aau.kah.bits.helpers.ConfigurationLoader;
-
 
 public class Bits {
 
-	public static void main(String[] args) {	
-		
+	public static void main(String[] args) {
+
 		DatabaseHandler databaseHandler;
 		DatabaseConfig databaseConfig;
-	
+
 		try {
-			//databaseConfig = ConfigurationLoader.load("onto0st-fact0st-dim0st.json");
-			databaseConfig = ConfigurationLoader.load("onto0st-fact1pt-dim#pt.json");
+			databaseConfig = ConfigurationLoader.loadDatabaseConfig("onto0st-fact1st-dim#st.json");
+			
+			//ConfigurationLoader.loadSystemConfig("kah-ThinkPad-X220.json");
+			GeneralConfig generalConfig = GeneralConfig.getInstance();
+			generalConfig.setTdbLoaderPath("/usr/local/apache-jena-2.12.1/bin/tdbloader");
+			generalConfig.setVerbose(true);
+		
 			databaseHandler = new DatabaseHandler(databaseConfig);
-			Model factModel = databaseHandler.getFactModel();
-			//PrintHelper.printModel(factModel);
+			
+			//System.out.println("all models: "+databaseHandler.getAllModelNames());
+			
+			ExperimentFactory experimentFactory = new ExperimentFactory();
+			AbstractExperimentHandler experimentHandler = experimentFactory.makeEvaluation(databaseHandler, databaseConfig);
+			experimentHandler.run();
+			experimentHandler.getResults();
+			
+			databaseHandler.closeConnection();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InvalidDatabaseConfig e) {
 			e.printStackTrace();
-		} 
-		
+		}
 
 		
-		//Query database
 		
-		//display/save results
+		// Query database
+
+		// display/save results
 
 	}
-	
-
 
 }
